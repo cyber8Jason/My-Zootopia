820271ae4b56a4a42cbd921be9a01a7e61509fa8
cyber8Jason <steiert.business@gmail.com>
2025-04-17 22:50:43 +0200

    added comments and updated some functions

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,50 +2,62 @@ import json
 
 
 def load_data(file_path):
-    """ Loads a JSON file """
+    """ loads a JSON file """
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
-def generate_animal_string(animals_data):
-    """ Prints selected information for each animal in the list """
+def serialize_animal(animal):
+    """
+    generates an HTML list item with selected information about one animal
+    :param animal: dictionary containing animal data
+    :return: HTML string for one animal card
+    """
     output = ""
-    for animal in animals_data:
+    output += "<li class='cards__item'>\n"
+    if "name" in animal:
         animal_name = animal['name'].replace("’", "'")
-        output += "<li class='cards__item'>\n"
-        if "name" in animal:
-            output += f"\t<div class='card__title'>{animal_name}</div>\n"
-        output += "\t\t<p class='card__text'>\n"
-        if "characteristics" in animal and "diet" in animal["characteristics"]:
-            output += f"\t\t\t<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
-        if "locations" in animal and animal["locations"]:
-            output += f"\t\t\t<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
-        if "characteristics" in animal and "type" in animal["characteristics"]:
-            output += f"\t\t\t<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
-        output += "\t\t</p>\n"
-        output += "</li>\n"
+        output += f"\t<div class='card__title'>{animal_name}</div>\n"
+    output += "\t\t<p class='card__text'>\n"
+    if "characteristics" in animal and "diet" in animal["characteristics"]:
+        output += f"\t\t\t<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
+    if "locations" in animal and animal["locations"]:
+        output += f"\t\t\t<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
+    if "characteristics" in animal and "type" in animal["characteristics"]:
+        output += f"\t\t\t<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
+    output += "\t\t</p>\n"
+    output += "</li>\n"
     return output
 
 
 def load_template(file_path):
+    """ loads the content of the template file and returns it as a string """
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
 
 
-def inject_animals_into_template(template, animals_string):
+def insert_animals_into_template(template, animals_string):
+    """ finds the placeholder __REPLACE_ANIMALS_INFO__ in the template and replaces it with the animal data string """
     return template.replace("__REPLACE_ANIMALS_INFO__", animals_string)
 
 
 def save_html(output_string, filename="animals.html"):
+    """ saves the final HTML content into a file with the filename animals.html """
     with open(filename, "w", encoding="utf-8") as file:
         file.write(output_string)
 
 
 def main():
+    """
+    generates an HTML file with animal info by
+    loading JSON data, formatting it, and inserting it into a template
+    """
     animals_data = load_data("animals_data.json")
-    animals_string = generate_animal_string(animals_data)
+    output = ""
+    for animal in animals_data:
+        output += serialize_animal(animal)
     template = load_template("animals_template.html")
-    final_html = inject_animals_into_template(template, animals_string)
+    final_html = insert_animals_into_template(template, output)
     save_html(final_html)
 
 
